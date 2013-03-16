8514fd2d505b8c0e3c9e507e5221580aff40c850
Thomas Stassin <thomas.stassin@gsmproductions.com>
2013-03-16 08:29:40 +0100

    mise à jour des backup

--- scripts_db/backup/dump_20130316.sql
+++ scripts_db/backup/dump_20130316.sql
@@ -39,6 +39,36 @@ LOCK TABLES `dungeon` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `enemy`
+--
+
+DROP TABLE IF EXISTS `enemy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `enemy` (
+  `ene_id` int(11) NOT NULL,
+  `ene_name` varchar(45) NOT NULL,
+  `ene_til_xid` int(11) NOT NULL,
+  `ene_til_x` int(11) NOT NULL,
+  `ene_til_y` int(11) NOT NULL,
+  PRIMARY KEY (`ene_id`),
+  UNIQUE KEY `ene_name_UNIQUE` (`ene_name`),
+  KEY `fk_tileset_fk2` (`ene_til_xid`),
+  CONSTRAINT `fk_tileset_fk2` FOREIGN KEY (`ene_til_xid`) REFERENCES `tileset` (`til_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `enemy`
+--
+
+LOCK TABLES `enemy` WRITE;
+/*!40000 ALTER TABLE `enemy` DISABLE KEYS */;
+INSERT INTO `enemy` VALUES (0,'imp',7,0,0);
+/*!40000 ALTER TABLE `enemy` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `object`
 --
 
@@ -48,13 +78,14 @@ DROP TABLE IF EXISTS `object`;
 CREATE TABLE `object` (
   `obj_id` int(11) NOT NULL AUTO_INCREMENT,
   `obj_name` varchar(45) NOT NULL,
+  `obj_type` varchar(20) NOT NULL,
   `obj_til_xid` int(11) NOT NULL,
   `obj_til_x` int(11) NOT NULL,
   `obj_til_y` int(11) NOT NULL,
   PRIMARY KEY (`obj_id`),
   KEY `fk_tileset_fk` (`obj_til_xid`),
   CONSTRAINT `fk_tileset_fk` FOREIGN KEY (`obj_til_xid`) REFERENCES `tileset` (`til_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,7 +94,7 @@ CREATE TABLE `object` (
 
 LOCK TABLES `object` WRITE;
 /*!40000 ALTER TABLE `object` DISABLE KEYS */;
-INSERT INTO `object` VALUES (1,'empty',2,0,0),(2,'poison trap',2,0,1);
+INSERT INTO `object` VALUES (4,'poison trap','trap',6,0,1),(5,'heal potion','item',8,0,0);
 /*!40000 ALTER TABLE `object` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,11 +111,12 @@ CREATE TABLE `room` (
   `roo_use_xid` int(11) NOT NULL,
   `roo_til_xid` int(11) NOT NULL,
   PRIMARY KEY (`roo_id`),
+  UNIQUE KEY `ndx_roo_name` (`roo_name`),
   KEY `fk_user_fk1` (`roo_use_xid`),
   KEY `fk_tileset_fk1` (`roo_til_xid`),
   CONSTRAINT `fk_tileset_fk1` FOREIGN KEY (`roo_til_xid`) REFERENCES `tileset` (`til_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_user_fk1` FOREIGN KEY (`roo_use_xid`) REFERENCES `user` (`use_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +125,7 @@ CREATE TABLE `room` (
 
 LOCK TABLES `room` WRITE;
 /*!40000 ALTER TABLE `room` DISABLE KEYS */;
-INSERT INTO `room` VALUES (1,'test',1,1);
+INSERT INTO `room` VALUES (3,'first_room',6,5);
 /*!40000 ALTER TABLE `room` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,6 +157,37 @@ LOCK TABLES `room_dungeon` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `room_enemy`
+--
+
+DROP TABLE IF EXISTS `room_enemy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `room_enemy` (
+  `ren_roo_xid` int(11) NOT NULL,
+  `ren_x` int(11) NOT NULL,
+  `ren_y` int(11) NOT NULL,
+  `ren_ene_xid` int(11) NOT NULL,
+  `ren_ene_lvl` int(11) NOT NULL,
+  PRIMARY KEY (`ren_roo_xid`,`ren_x`,`ren_y`),
+  KEY `fk_room_fk2` (`ren_roo_xid`),
+  KEY `fk_enemy_fk1` (`ren_ene_xid`),
+  CONSTRAINT `fk_room_fk2` FOREIGN KEY (`ren_roo_xid`) REFERENCES `room` (`roo_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_enemy_fk1` FOREIGN KEY (`ren_ene_xid`) REFERENCES `enemy` (`ene_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room_enemy`
+--
+
+LOCK TABLES `room_enemy` WRITE;
+/*!40000 ALTER TABLE `room_enemy` DISABLE KEYS */;
+INSERT INTO `room_enemy` VALUES (3,3,3,0,1),(3,5,2,0,1),(3,10,10,0,1);
+/*!40000 ALTER TABLE `room_enemy` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `room_object`
 --
 
@@ -150,7 +213,7 @@ CREATE TABLE `room_object` (
 
 LOCK TABLES `room_object` WRITE;
 /*!40000 ALTER TABLE `room_object` DISABLE KEYS */;
-INSERT INTO `room_object` VALUES (1,'0','1',2),(1,'3','4',2);
+INSERT INTO `room_object` VALUES (3,1,1,4),(3,5,10,4),(3,10,12,4),(3,6,6,5),(3,7,1,5);
 /*!40000 ALTER TABLE `room_object` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,11 +228,9 @@ CREATE TABLE `tileset` (
   `til_id` int(11) NOT NULL AUTO_INCREMENT,
   `til_name` varchar(45) NOT NULL,
   `til_path` varchar(100) NOT NULL,
-  `til_tty_xid` int(11) NOT NULL,
-  PRIMARY KEY (`til_id`),
-  KEY `fk_tileset_type_fk1` (`til_tty_xid`),
-  CONSTRAINT `fk_tileset_type_fk1` FOREIGN KEY (`til_tty_xid`) REFERENCES `tileset_type` (`tty_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `til_type` varchar(100) NOT NULL,
+  PRIMARY KEY (`til_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -178,32 +239,8 @@ CREATE TABLE `tileset` (
 
 LOCK TABLES `tileset` WRITE;
 /*!40000 ALTER TABLE `tileset` DISABLE KEYS */;
-INSERT INTO `tileset` VALUES (1,'room','img/tileset/dungeon.png',1),(2,'object','img/tileset/object.png',2);
+INSERT INTO `tileset` VALUES (5,'room','img/tileset/dungeon.png','room'),(6,'trap','img/tileset/traps.png','item'),(7,'imp','img/tileset/imp.png','enemy'),(8,'item','img/tileset/items.png','room');
 /*!40000 ALTER TABLE `tileset` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tileset_type`
---
-
-DROP TABLE IF EXISTS `tileset_type`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tileset_type` (
-  `tty_id` int(11) NOT NULL,
-  `tty_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`tty_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tileset_type`
---
-
-LOCK TABLES `tileset_type` WRITE;
-/*!40000 ALTER TABLE `tileset_type` DISABLE KEYS */;
-INSERT INTO `tileset_type` VALUES (1,'room'),(2,'object');
-/*!40000 ALTER TABLE `tileset_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -217,8 +254,9 @@ CREATE TABLE `user` (
   `use_id` int(11) NOT NULL AUTO_INCREMENT,
   `use_nickname` varchar(45) NOT NULL,
   `use_password` varchar(45) NOT NULL,
-  PRIMARY KEY (`use_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`use_id`),
+  UNIQUE KEY `ndx_use_name` (`use_nickname`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -227,7 +265,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'sempai','pwd'),(2,'test','test'),(3,'test2','test'),(4,'test3','test');
+INSERT INTO `user` VALUES (6,'sempai','');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -240,4 +278,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-03-10  7:55:03
+-- Dump completed on 2013-03-16  8:28:26
