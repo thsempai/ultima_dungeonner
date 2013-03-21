3810c93372689e27de5d72255c9b80f3d54f1cb3
Thomas Stassin <thomas.stassin@gsmproductions.com>
2013-03-22 00:11:17 +0100

    mise à jour des modules

--- sources/main.py
+++ sources/main.py
@@ -6,6 +6,7 @@ from dungeon import Dungeon, RoomScene
 from initialization import init
 from db_connection import DBConnection
 from user import User
+from ud_exception import UDungeonException
 
 SCREEN_SIZE = 800,600
 
@@ -13,16 +14,19 @@ def test():
     title = "Ultima Dungeonner alpha v0.01"
     game = cocos.director.director.init(width=SCREEN_SIZE[0], height=SCREEN_SIZE[1], caption=title)
     
-    user = DBConnection.getUser(getpass.getuser())
+    try:
+        user = DBConnection.getUser(getpass.getuser())
 
-    user = User(user)
-    hero = user.getHero()
+        user = User(user)
+        hero = user.getHero()
 
-    dungeon = Dungeon(hero)
+        dungeon = Dungeon(hero)
 
-    game.push_handlers(dungeon)
-    
-    cocos.director.director.run(dungeon[0])
+        game.push_handlers(dungeon)
+        
+        cocos.director.director.run(dungeon[0])
+    except UDungeonException as ude:
+        print ude.message
 
 if __name__ == "__main__":
     init()
