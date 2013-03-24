34c515f6505cf3c386eee2766cf7ccd9f7b08deb
Thomas Stassin <thomas.stassin@gsmproductions.com>
2013-03-24 13:06:09 +0100

    ajout du nouveau module

---
+++ sources/menu.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import cocos
+
+MENU_TRANSITION = cocos.scenes.transitions.ZoomTransition
+
+class MainMenu(cocos.menu.Menu):
+
+    def __init__(self,commands):
+
+        cocos.menu.Menu.__init__(self)
+
+        self.font_item['font_name'] = 'Drakoheart Leiend'            
+        self.font_item['font_size'] = 50
+        self.font_item_selected['font_name'] = 'Drakoheart Leiend' 
+        self.font_item_selected['font_size'] = 50  
+        self.menu_valign = cocos.menu.BOTTOM
+        self.menu_halign = cocos.menu.CENTER
+        self.y += 100
+
+        l = []
+
+        for cmd in commands:
+            l.append(cocos.menu.MenuItem(cmd[0], cmd[1],*cmd[2]))
+
+        self.create_menu(l, None, None)
+
+    def on_quit(self):
+              pass
