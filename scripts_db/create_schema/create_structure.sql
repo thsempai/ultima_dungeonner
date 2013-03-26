67e091d6330a14aca47f7f5a55fdf4c80ce31155
Thomas Stassin <thomas.stassin@gsmproductions.com>
2013-03-26 23:10:32 +0100

    mise à jour des scriot db

--- scripts_db/create_schema/create_structure.sql
+++ scripts_db/create_schema/create_structure.sql
@@ -7,18 +7,32 @@ CREATE SCHEMA IF NOT EXISTS `1gam201302` DEFAULT CHARACTER SET utf8 ;
 USE `1gam201302` ;
 
 -- -----------------------------------------------------
+-- Table `1gam201302`.`data_site`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `1gam201302`.`data_site` (
+  `das_id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `das_field` VARCHAR(45) NOT NULL ,
+  `das_data` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`das_id`) ,
+  UNIQUE INDEX `das_field_UNIQUE` (`das_field` ASC) )
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = big5;
+
+
+-- -----------------------------------------------------
 -- Table `1gam201302`.`user`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `1gam201302`.`user` (
   `use_id` INT(11) NOT NULL AUTO_INCREMENT ,
   `use_nickname` VARCHAR(45) NOT NULL ,
   `use_password` VARCHAR(45) NOT NULL ,
-  `use_last_connection` DATETIME NULL ,
+  `use_last_connection` DATETIME NULL DEFAULT NULL ,
   `use_creation` DATETIME NOT NULL ,
   PRIMARY KEY (`use_id`) ,
   UNIQUE INDEX `ndx_use_name` (`use_nickname` ASC) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 6
+AUTO_INCREMENT = 16
 DEFAULT CHARACTER SET = utf8;
 
 
@@ -50,7 +64,29 @@ CREATE  TABLE IF NOT EXISTS `1gam201302`.`tileset` (
   `til_type` VARCHAR(100) NOT NULL ,
   PRIMARY KEY (`til_id`) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 5
+AUTO_INCREMENT = 9
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `1gam201302`.`enemy`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `1gam201302`.`enemy` (
+  `ene_id` INT(11) NOT NULL ,
+  `ene_name` VARCHAR(45) NOT NULL ,
+  `ene_til_xid` INT(11) NOT NULL ,
+  `ene_til_x` INT(11) NOT NULL ,
+  `ene_til_y` INT(11) NOT NULL ,
+  `ene_description` VARCHAR(100) NOT NULL ,
+  PRIMARY KEY (`ene_id`) ,
+  UNIQUE INDEX `ene_name_UNIQUE` (`ene_name` ASC) ,
+  INDEX `fk_tileset_fk2` (`ene_til_xid` ASC) ,
+  CONSTRAINT `fk_tileset_fk2`
+    FOREIGN KEY (`ene_til_xid` )
+    REFERENCES `1gam201302`.`tileset` (`til_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
@@ -73,7 +109,7 @@ CREATE  TABLE IF NOT EXISTS `1gam201302`.`object` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-AUTO_INCREMENT = 4
+AUTO_INCREMENT = 6
 DEFAULT CHARACTER SET = utf8;
 
 
@@ -100,7 +136,7 @@ CREATE  TABLE IF NOT EXISTS `1gam201302`.`room` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-AUTO_INCREMENT = 3
+AUTO_INCREMENT = 5
 DEFAULT CHARACTER SET = utf8;
 
 
@@ -121,6 +157,32 @@ CREATE  TABLE IF NOT EXISTS `1gam201302`.`room_dungeon` (
   CONSTRAINT `fk_room_fk1`
     FOREIGN KEY (`rod_roo_xid` )
     REFERENCES `1gam201302`.`room` (`roo_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `1gam201302`.`room_enemy`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `1gam201302`.`room_enemy` (
+  `ren_roo_xid` INT(11) NOT NULL ,
+  `ren_x` INT(11) NOT NULL ,
+  `ren_y` INT(11) NOT NULL ,
+  `ren_ene_xid` INT(11) NOT NULL ,
+  `ren_ene_lvl` INT(11) NOT NULL ,
+  PRIMARY KEY (`ren_roo_xid`, `ren_x`, `ren_y`) ,
+  INDEX `fk_room_fk2` (`ren_roo_xid` ASC) ,
+  INDEX `fk_enemy_fk1` (`ren_ene_xid` ASC) ,
+  CONSTRAINT `fk_room_fk2`
+    FOREIGN KEY (`ren_roo_xid` )
+    REFERENCES `1gam201302`.`room` (`roo_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_enemy_fk1`
+    FOREIGN KEY (`ren_ene_xid` )
+    REFERENCES `1gam201302`.`enemy` (`ene_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -153,77 +215,85 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `1gam201302`.`enemy`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `1gam201302`.`enemy` (
-  `ene_id` INT NOT NULL ,
-  `ene_name` VARCHAR(45) NOT NULL ,
-  `ene_til_xid` INT(11) NOT NULL ,
-  `ene_til_x` INT NOT NULL ,
-  `ene_til_y` INT NOT NULL ,
-  `ene_description` VARCHAR(100) NOT NULL ,
-  PRIMARY KEY (`ene_id`) ,
-  UNIQUE INDEX `ene_name_UNIQUE` (`ene_name` ASC) ,
-  INDEX `fk_tileset_fk2` (`ene_til_xid` ASC) ,
-  CONSTRAINT `fk_tileset_fk2`
-    FOREIGN KEY (`ene_til_xid` )
-    REFERENCES `1gam201302`.`tileset` (`til_id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `1gam201302`.`room_enemy`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `1gam201302`.`room_enemy` (
-  `ren_roo_xid` INT NOT NULL ,
-  `ren_x` INT NOT NULL ,
-  `ren_y` INT NOT NULL ,
-  `ren_ene_xid` INT NOT NULL ,
-  `ren_ene_lvl` INT NOT NULL ,
-  PRIMARY KEY (`ren_roo_xid`, `ren_x`, `ren_y`) ,
-  INDEX `fk_room_fk2` (`ren_roo_xid` ASC) ,
-  INDEX `fk_enemy_fk1` (`ren_ene_xid` ASC) ,
-  CONSTRAINT `fk_room_fk2`
-    FOREIGN KEY (`ren_roo_xid` )
-    REFERENCES `1gam201302`.`room` (`roo_id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_enemy_fk1`
-    FOREIGN KEY (`ren_ene_xid` )
-    REFERENCES `1gam201302`.`enemy` (`ene_id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
 -- Table `1gam201302`.`update_file`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `1gam201302`.`update_file` (
-  `upf_id` INT NOT NULL AUTO_INCREMENT ,
+  `upf_id` INT(11) NOT NULL AUTO_INCREMENT ,
   `upf_path` VARCHAR(45) NOT NULL ,
   `upf_update_date` DATETIME NOT NULL ,
   PRIMARY KEY (`upf_id`) ,
   UNIQUE INDEX `upf_path_UNIQUE` (`upf_path` ASC) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `1gam201302`.`data_site`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `1gam201302`.`data_site` (
-  `das_id` INT NOT NULL AUTO_INCREMENT ,
-  `das_field` VARCHAR(45) NOT NULL ,
-  `das_data` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`das_id`) ,
-  UNIQUE INDEX `das_field_UNIQUE` (`das_field` ASC) )
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = big5
-COLLATE = big5_chinese_ci;
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
 
+
+-- -----------------------------------------------------
+-- function get_dungeon
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `1gam201302`$$
+CREATE DEFINER=`thsempai`@`%` FUNCTION `get_dungeon`(nickname varchar(45)) RETURNS int(11)
+begin
+
+        declare dungeon_id int;
+        declare user_id int;
+
+        
+
+        select 
+            get_user(use_nickname)
+        into
+            user_id;
+
+        return 1;
+
+    end$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- function get_user
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `1gam201302`$$
+CREATE DEFINER=`thsempai`@`%` FUNCTION `get_user`(nickname varchar(45)) RETURNS int(11)
+begin
+
+    declare user_id int;
+
+    if not exists(select use_id from user where use_nickname = nickname) then
+
+        insert into
+            user
+                (
+                use_nickname,
+                use_password,
+                use_creation
+                )
+            values
+                (
+                nickname,
+                '',
+                curdate()
+                );
+    end if;
+
+    select 
+        use_id 
+    into 
+        user_id
+    from 
+        user 
+    where 
+        use_nickname = nickname;
+
+    return user_id;
+    end$$
+
+DELIMITER ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
